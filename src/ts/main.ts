cc7585b1bbfaaef8c081b2e49f3a513be6c0a62f
Takatsu-yuki <yk.neve@gmail.com>
2024-11-18 01:03:12 +0900

    nav 調整

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,20 @@
-console.log("hello world!");
+// 'use strict';
+
+// //navigation
+// const navigation = document.querySelector('.navigation');
+// const humbergarBtn = document.getElementById('humbergar-btn');
+// const navigationTitle = document.querySelectorAll('.navigation-title');
+
+// humbergarBtn.addEventListener('click',function(){
+//   humbergarBtn.classList.toggle('open');
+//   navigation.classList.toggle('open')
+// });
+
+// for(let i = 0; i < navigationTitle.length; i ++ ){
+// navigationTitle[i].addEventListener('click',function(){
+//   humbergarBtn.classList.remove('open');
+//   navigation.classList.remove('open')
+// });
+// }
+
+// console.log(humbergarBtn);
