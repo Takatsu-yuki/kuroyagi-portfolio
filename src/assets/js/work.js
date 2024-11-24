e266d7a18dff4b8a7adeebb0f842582891ea392d
Takatsu-yuki <yk.neve@gmail.com>
2024-11-25 01:07:19 +0900

    ボタン修正

--- src/assets/js/work.js
+++ src/assets/js/work.js
@@ -4,14 +4,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   var grid = new Muuri(".grid");
 
-  console.log(document.querySelector(".filter-buttons button.active"));
-  document.querySelectorAll(".filter-btns button").forEach((button) => {
-    button.addEventListener("click", () => {
-      const filter = button.dataset.filter; // ボタンのdata-filter属性からフィルタを取得
+  console.log(document.querySelector(".filter-buttons span.active"));
+  document.querySelectorAll(".filter-btns span").forEach((span) => {
+    span.addEventListener("click", () => {
+      const filter = span.dataset.filter; // ボタンのdata-filter属性からフィルタを取得
       grid.filter(filter === "*" ? () => true : `[data-tag="${filter}"]`); // data-tag属性でフィルタリング
 
-      document.querySelector(".filter-btns button.active")?.classList.remove("active");
-      button.classList.add("active");
+      document.querySelector(".filter-btns span.active")?.classList.remove("active");
+      span.classList.add("active");
     });
   });
 });
